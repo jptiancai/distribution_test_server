61bb92e595e6dbe8e11a9a08cb60f307ea0d211d
jptiancai <mail_jzy@163.com>
2020-01-04 22:41:20 +0800

    add run csv test case

---
+++ test/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    pytest_html = item.config.pluginmanager.getplugin('html')
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, 'extra', [])
+    if report.when == 'call':
+        # always add url to report
+        extra.append(pytest_html.extras.url('http://www.example.com/'))
+        xfail = hasattr(report, 'wasxfail')
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            # only add additional html on failure
+            extra.append(pytest_html.extras.html('<div>Additional HTML</div>'))
+            extra.append(pytest_html.extras.json({'name': 'pytest'}))
+            extra.append(pytest_html.extras.image('/data/app/distribution_test_server/reports/img/TestResults___main__.TestMethods_2019-12-21_23-02-09.png'))
+        report.extra = extra
